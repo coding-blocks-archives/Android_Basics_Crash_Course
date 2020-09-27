163d9cff04f36d9ce4de53a51d35ecbe2d0daf7b
Arnav Gupta <arnav@codingblocks.com>
2020-09-27 07:35:13 +0530

    adding numbers
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- 02_UI_Interactions/app/src/main/java/com/codingblocks/uiinteractions/MainActivity.kt
+++ 02_UI_Interactions/app/src/main/java/com/codingblocks/uiinteractions/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.codingblocks.uiinteractions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener {
+            val var1 = etVar1.text.toString().toInt()
+            val var2 = etVar2.text.toString().toInt()
+
+            val result = (var1 + var2).toString()
+
+            tvResult.text = result
+        }
     }
 }
