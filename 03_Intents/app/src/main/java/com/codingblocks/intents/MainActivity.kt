541a904d4331bc8ebb3d6df648dc2f349b313c40
Arnav Gupta <arnav@codingblocks.com>
2020-09-27 14:25:38 +0530

    from one activity to another
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ 03_Intents/app/src/main/java/com/codingblocks/intents/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.codingblocks.intents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGoRed.setOnClickListener {
+            val intent = Intent(this, RedActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ 03_Intents/app/src/main/java/com/codingblocks/intents/RedActivity.kt
@@ -0,0 +1,11 @@
+package com.codingblocks.intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_red)
+    }
+}
