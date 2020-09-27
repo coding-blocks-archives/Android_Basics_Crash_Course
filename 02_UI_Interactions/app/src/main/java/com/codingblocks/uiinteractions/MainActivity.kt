d3cf840ae0d477b4e17edcfe3c33a54d5c897680
Arnav Gupta <arnav@codingblocks.com>
2020-09-27 14:03:13 +0530

    Error handling via dialogs
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- 02_UI_Interactions/app/src/main/java/com/codingblocks/uiinteractions/MainActivity.kt
+++ 02_UI_Interactions/app/src/main/java/com/codingblocks/uiinteractions/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.codingblocks.uiinteractions
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnAdd.setOnClickListener {
-            val var1 = etVar1.text.toString().toInt()
-            val var2 = etVar2.text.toString().toInt()
+            val var1: Int
+            val var2: Int
+
+            try {
+                var1 = etVar1.text.toString().toInt()
+                var2 = etVar2.text.toString().toInt()
+            } catch (e: NumberFormatException) {
+                AlertDialog.Builder(this)
+                        .setTitle("Invalid Operation")
+                        .setMessage("Please enter both numbers to calculate")
+                        .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                        .setCancelable(false)
+                        .show()
+
+
+                return@setOnClickListener
+            }
 
             val result = (var1 + var2).toString()
 
