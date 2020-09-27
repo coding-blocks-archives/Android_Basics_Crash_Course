168e8f12c2cd4e1710c711131ca40947a8daf1b1
Arnav Gupta <arnav@codingblocks.com>
2020-09-27 15:22:54 +0530

    implicit intents
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- 03_Intents/app/src/main/java/com/codingblocks/intents/MainActivity.kt
+++ 03_Intents/app/src/main/java/com/codingblocks/intents/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codingblocks.intents
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,8 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnGoRed.setOnClickListener {
-            val intent = Intent(this, RedActivity::class.java)
+        btnDial.setOnClickListener {
+            val phNo = etPhNo.text.toString()
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phNo"))
+            startActivity(intent)
+        }
+
+        btnBrowse.setOnClickListener {
+            val url = etUrl.text.toString()
+
+            if (url.isEmpty()) {
+                // Dialog to tell user to enter an URL
+            } else if (!url.startsWith("http")) {
+                // either prefix http:// or ask user to enter full URL
+            }
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
     }
