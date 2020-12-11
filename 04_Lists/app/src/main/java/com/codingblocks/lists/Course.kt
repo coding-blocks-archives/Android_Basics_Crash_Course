ad8a34d295115e950070d4e2c04a4e26f7ceccb9
Arnav Gupta <arnav@codingblocks.com>
2020-12-12 04:25:02 +0530

    update course list
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ 04_Lists/app/src/main/java/com/codingblocks/lists/Course.kt
@@ -0,0 +1,9 @@
+package com.codingblocks.lists
+
+data class Course(
+    val name: String,
+    val teacher: String,
+    val lectures: Int,
+    val fees: Int
+)
+
--- 04_Lists/app/src/main/java/com/codingblocks/lists/MainActivity.kt
+++ 04_Lists/app/src/main/java/com/codingblocks/lists/MainActivity.kt
@@ -1,33 +1,49 @@
 package com.codingblocks.lists
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item_course.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val courses = listOf(
-            "Android", "NodeJS", "Django",
-            "Competitive Programming", "Dynamic Programming",
-            "C++", "Java", "Data Structures",
-            "Interview Prep", "React", "VueJS",
-            "FAANG Interview", "Machine Learning",
-            "Data Science", "Natural Language Processing",
-            "Kotlin Basics"
+    private val courses = listOf<Course>(
+        Course("Android", "Arnav", 20, 14999),
+        Course("NodeJS", "Arnav", 25, 16999),
+        Course("Recursion", "Rishab", 10, 9999),
+        Course("Dynamic Programming", "Prateek", 15, 12999),
+        Course("Java Basics", "Garima", 10, 6999),
+        Course("C++ Basics", "Pranav", 10, 6999),
+        Course("Kotlin", "Pulkit", 7, 999),
+        Course("Javascript", "Sanjeet", 8, 1299),
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lvCourses.adapter = ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                courses
-        )
+        lvCourses.adapter = object : BaseAdapter() {
+            override fun getCount() = courses.size
+            override fun getItem(position: Int): Course = courses[position]
+
+            override fun getItemId(position: Int): Long = getItem(position).hashCode().toLong()
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val itemView = layoutInflater.inflate(R.layout.list_item_course, parent, false)
+                val course = getItem(position)
+                itemView.tvCourseName.text = course.name
+                itemView.tvTeacherName.text = course.teacher
+                itemView.tvCourseDetails.text = "Lectures ${course.lectures} | Fees: INR ${course.fees}"
+                return itemView
+            }
+
+        }
         lvCourses.setOnItemClickListener { _, _, position, _ ->
             Toast.makeText(this, "Clicked on ${courses[position]}", Toast.LENGTH_SHORT).show()
         }
